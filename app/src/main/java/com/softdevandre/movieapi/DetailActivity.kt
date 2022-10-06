c64aa8c38f80702a61268403aa635c13a1435227
softdevandre <softdev.andre@gmail.com>
2022-10-05 21:31:04 -0300

    created the activity for the movie detail item layout

---
+++ app/src/main/java/com/softdevandre/movieapi/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.softdevandre.movieapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+
+
+}
