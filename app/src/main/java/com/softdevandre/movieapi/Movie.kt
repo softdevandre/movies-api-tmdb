c3cb341c2415ce8c218441d67e398b4fc969cf2e
softdevandre <softdev.andre@gmail.com>
2022-10-10 08:53:39 -0300

    changed class and setted it up to get the values from the API's endpoint arraylist

---
+++ app/src/main/java/com/softdevandre/movieapi/Movie.kt
@@ -0,0 +1,22 @@
+package com.softdevandre.movieapi
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Movie(
+    @SerializedName("id")
+    val id: String?,
+
+    @SerializedName("title")
+    val title: String?,
+
+    @SerializedName("poster_path")
+    val poster: String?,
+
+    @SerializedName("release_date")
+    val release: String?
+) : Parcelable {
+    constructor() : this("", "", "", "")
+}
