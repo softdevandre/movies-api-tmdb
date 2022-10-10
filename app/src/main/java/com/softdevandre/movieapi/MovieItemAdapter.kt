0d8634760e83069cab1ea5d4372c0316c015f851
softdevandre <softdev.andre@gmail.com>
2022-10-10 08:59:37 -0300

    updated the adapter to bind properly the items from the API

--- app/src/main/java/com/softdevandre/movieapi/MovieItemAdapter.kt
+++ app/src/main/java/com/softdevandre/movieapi/MovieItemAdapter.kt
@@ -1,43 +1,38 @@
 package com.softdevandre.movieapi
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.card.MaterialCardView
+import com.bumptech.glide.Glide
 import com.softdevandre.movieapi.databinding.MovieItemBinding
 
-class MovieItemAdapter (private val context: Context, private val dataset: List<Movies>) :
+class MovieItemAdapter(private val context: Context, private val dataset: List<Movie>) :
     RecyclerView.Adapter<MovieItemAdapter.MovieViewHolder>() {
 
-    class MovieViewHolder(binding: MovieItemBinding) :
+    class MovieViewHolder(private val binding: MovieItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        val textViewName: TextView = binding.tvMovieName
-        val textViewYear: TextView = binding.tvMovieYear
-        val imageView: ImageView = binding.ivMovie
-        val materialCard: MaterialCardView = binding.mcvMovie
+
+        fun bind(movie: Movie) = with(binding) {
+            binding.tvMovieTitle.text = movie.title
+            binding.tvMovieReleaseDate.text = movie.release
+            Glide.with(itemView).load(IMAGE_BASE + movie.poster).into(binding.ivMovie)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        // create new view
-        val adapterLayout =
+        return MovieViewHolder(
             MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MovieViewHolder(adapterLayout)
+        )
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        val item = dataset[position]
-        holder.textViewName.text = context.resources.getString(item.movieName)
-        holder.textViewYear.text = context.resources.getString(item.movieYear)
-        holder.imageView.setImageResource(item.movieImage)
-//        holder.materialCard.setOnClickListener {
-//            val intent = Intent(context, DetailCharacterActivity::class.java)
-//            context.startActivity(intent)
-//        }
+        holder.bind(dataset[position])
     }
 
     override fun getItemCount() = dataset.size
+
+    companion object {
+        private const val IMAGE_BASE = "https://image.tmdb.org/t/p/w500/"
+    }
 }
