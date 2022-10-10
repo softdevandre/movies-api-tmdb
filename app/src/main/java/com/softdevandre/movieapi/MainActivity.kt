6374a3803796bfa85ecfacff15874e39034190bf
softdevandre <softdev.andre@gmail.com>
2022-10-10 09:09:24 -0300

    updated recyclerview and adapter's reference and setted up new code to show list items from API's endpoint

--- app/src/main/java/com/softdevandre/movieapi/MainActivity.kt
+++ app/src/main/java/com/softdevandre/movieapi/MainActivity.kt
@@ -2,7 +2,11 @@ package com.softdevandre.movieapi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.softdevandre.movieapi.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +18,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val myDataset = MoviesDatasource().loadMovies()
-
         val recyclerView = binding.rvMovieList
-        recyclerView.adapter = MovieItemAdapter(this, myDataset)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.setHasFixedSize(true)
+        getMovieData { movies: List<Movie> ->
+            recyclerView.adapter = MovieItemAdapter(this, movies)
+        }
+    }
+
+    private fun getMovieData(callback: (List<Movie>) -> Unit) {
+        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
+        apiService.getMoviesList().enqueue(object : Callback<MovieResponse> {
+            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+                return callback(response.body()!!.movies)
+            }
+
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+
+            }
+        })
     }
 }
