ceac7c3d0d9c225b249aaaba18798c751298c6cc
softdevandre <softdev.andre@gmail.com>
2022-10-10 08:57:13 -0300

    setted up API's base url and retrofit instance

---
+++ app/src/main/java/com/softdevandre/movieapi/MovieApiService.kt
@@ -0,0 +1,22 @@
+package com.softdevandre.movieapi
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MovieApiService {
+
+    companion object {
+        private const val BASE_URL = "https://api.themoviedb.org/3/"
+        private var retrofit: Retrofit? = null
+
+        fun getInstance(): Retrofit {
+            if (retrofit == null){
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit!!
+        }
+    }
+}
