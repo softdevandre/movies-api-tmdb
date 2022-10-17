91551da0402c9406b0bea898caa3565fd29a756c
softdevandre <softdev.andre@gmail.com>
2022-10-17 00:29:28 -0300

    setted up the intent to get the views from MovieItemAdapter to populate the activity detail layout

--- app/src/main/java/com/softdevandre/movieapi/DetailActivity.kt
+++ app/src/main/java/com/softdevandre/movieapi/DetailActivity.kt
@@ -1,13 +1,26 @@
 package com.softdevandre.movieapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.softdevandre.movieapi.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+
+    private val binding by lazy {
+        ActivityDetailBinding.inflate(layoutInflater)
     }
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        val intent = intent
+        val movieTitle = intent.getStringExtra("title")
+        val movieDate = intent.getStringExtra("release")
+        val moviePoster = intent.getStringExtra("poster")
+        binding.tvMovieTitle.text = movieTitle
+        binding.tvMovieReleaseDate.text = movieDate
+        Glide.with(this).load(moviePoster).into(binding.ivMovie)
+    }
 }
+
