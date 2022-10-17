0ec52da48bfe2ae3028f6d55a3ade057895be742
softdevandre <softdev.andre@gmail.com>
2022-10-17 00:25:25 -0300

    updated adapter to show the views in the DetailActivity

--- app/src/main/java/com/softdevandre/movieapi/ui/MovieItemAdapter.kt
+++ app/src/main/java/com/softdevandre/movieapi/ui/MovieItemAdapter.kt
@@ -4,24 +4,23 @@ import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.softdevandre.movieapi.DetailActivity
-import com.softdevandre.movieapi.model.Movie
 import com.softdevandre.movieapi.databinding.MovieItemBinding
+import com.softdevandre.movieapi.model.Movie
 import kotlinx.android.synthetic.main.movie_item.view.*
 
 class MovieItemAdapter(private val context: Context, private val dataset: List<Movie>) :
     RecyclerView.Adapter<MovieItemAdapter.MovieViewHolder>() {
 
-    class MovieViewHolder(private val binding: MovieItemBinding) :
+    class MovieViewHolder(binding: MovieItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(movie: Movie) = with(binding) {
-            binding.tvMovieTitle.text = movie.title
-            binding.tvMovieReleaseDate.text = movie.release
-            Glide.with(itemView).load(IMAGE_BASE + movie.poster).into(binding.ivMovie)
-        }
+        val movieTitle: TextView = binding.tvMovieTitle
+        val movieDate: TextView = binding.tvMovieReleaseDate
+        val moviePoster: ImageView = binding.ivMovie
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
@@ -32,10 +31,16 @@ class MovieItemAdapter(private val context: Context, private val dataset: List<M
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val item = dataset[position]
-        holder.bind(item)
+        Glide.with(holder.itemView.context)
+            .load(IMAGE_BASE + item.poster)
+            .into(holder.moviePoster)
+        holder.movieTitle.text = item.title
+        holder.movieDate.text = item.release
         holder.itemView.mcvMovie.setOnClickListener {
             val intent = Intent(context, DetailActivity::class.java)
-            intent.putExtra(DetailActivity.Extras.MOVIE, item)
+            intent.putExtra("title", item.title)
+            intent.putExtra("release", item.release)
+            intent.putExtra("poster", IMAGE_BASE + item.poster)
             context.startActivity(intent)
         }
     }
