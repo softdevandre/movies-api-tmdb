7905f08d1b0096990d7bd496a503d1cb480c58f5
softdevandre <softdev.andre@gmail.com>
2022-11-10 22:29:53 -0300

    refactored and changed code from main activity to support navigation components

--- app/src/main/java/com/softdevandre/movieapi/MainActivity.kt
+++ app/src/main/java/com/softdevandre/movieapi/MainActivity.kt
@@ -2,16 +2,9 @@ package com.softdevandre.movieapi
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
-import com.softdevandre.movieapi.data.MovieApiInterface
-import com.softdevandre.movieapi.data.MovieApiService
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.softdevandre.movieapi.databinding.ActivityMainBinding
-import com.softdevandre.movieapi.model.Movie
-import com.softdevandre.movieapi.model.MovieResponse
-import com.softdevandre.movieapi.ui.MovieItemAdapter
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,28 +12,14 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val recyclerView = binding.rvMovieList
-        recyclerView.layoutManager = GridLayoutManager(this, 2)
-        recyclerView.setHasFixedSize(true)
-        getMovieData { movies: List<Movie> ->
-            recyclerView.adapter = MovieItemAdapter(this, movies)
-        }
-    }
-
-    private fun getMovieData(callback: (List<Movie>) -> Unit) {
-        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
-        apiService.getMoviesList().enqueue(object : Callback<MovieResponse> {
-            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
-                return callback(response.body()!!.movies)
-            }
-
-            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-
-            }
-        })
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
