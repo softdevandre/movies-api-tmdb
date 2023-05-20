d5504cfb81f26f651cf8d0b6e74d53d41c4c69e1
softdevandre <softdev.andre@gmail.com>
2023-05-20 11:04:55 -0300

    refactor MovieDetailFragment

---
+++ app/src/main/java/com/softdevandre/movieapi/ui/MovieDetailFragment.kt
@@ -0,0 +1,26 @@
+package com.softdevandre.movieapi.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.softdevandre.movieapi.databinding.FragmentMovieDetailBinding
+
+class MovieDetailFragment : DialogFragment() {
+
+    private val viewModel: MovieViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentMovieDetailBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        binding.rvCast.adapter = MovieDetailAdapter()
+        viewModel.getMovie(arguments?.getInt("movie_id") ?: 0)
+
+        return binding.root
+    }
+}
