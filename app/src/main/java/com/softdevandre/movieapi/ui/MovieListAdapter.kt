c4445f527ee2ea9666c7fd61e6f73a561f6fa14f
softdevandre <softdev.andre@gmail.com>
2023-05-20 11:05:56 -0300

    refactor MovieListAdapter

--- app/src/main/java/com/softdevandre/movieapi/ui/MovieListAdapter.kt
+++ app/src/main/java/com/softdevandre/movieapi/ui/MovieListAdapter.kt
@@ -1,50 +1,50 @@
 package com.softdevandre.movieapi.ui
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.softdevandre.movieapi.MovieListFragmentDirections
-import com.softdevandre.movieapi.databinding.MovieItemBinding
-import com.softdevandre.movieapi.model.Movie
-import kotlinx.android.synthetic.main.movie_item.view.*
+import com.softdevandre.movieapi.databinding.MovieListItemBinding
+import com.softdevandre.movieapi.network.Movie
 
-class MovieListAdapter(private val context: Context, private val dataset: List<Movie>) :
-    RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>() {
+class MovieListAdapter(private val clickListener: MovieListener) :
+    ListAdapter<Movie, MovieListAdapter.MovieViewHolder>(DiffCallback) {
 
-    class MovieViewHolder(binding: MovieItemBinding) :
+    class MovieViewHolder(private var binding: MovieListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        val movieTitle: TextView = binding.tvMovieTitle
-        val movieDate: TextView = binding.tvMovieReleaseDate
-        val moviePoster: ImageView = binding.ivMovie
+        fun bind(clickListener: MovieListener, movie: Movie) {
+            binding.movie = movie
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Movie>() {
+        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem == newItem
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         return MovieViewHolder(
-            MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            MovieListItemBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
         )
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        val item = dataset[position]
-        Glide.with(holder.itemView.context)
-            .load(IMAGE_BASE + item.poster)
-            .into(holder.moviePoster)
-        holder.movieTitle.text = item.title
-        holder.movieDate.text = item.release
-        holder.itemView.mcvMovie.setOnClickListener {
-            val action = MovieListFragmentDirections.actionMovieListFragmentToDetailFragment()
-            holder.itemView.findNavController().navigate(action)
-        }
+        val movie = getItem(position)
+        holder.bind(clickListener, movie)
     }
 
-    override fun getItemCount() = dataset.size
+}
 
-    companion object {
-        const val IMAGE_BASE = "https://image.tmdb.org/t/p/w500/"
-    }
+class MovieListener(val clickListener: (movie: Movie) -> Unit) {
+    fun onClick(movie: Movie) = clickListener(movie)
 }
